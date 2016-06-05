f6c4f956d486f6f1e904d01682fd1d026f3f7385
Mike Gibson <mike@mgdigital.co.uk>
2016-06-05 16:01:55 +0100

    Don't inject container

--- src/Console/AbstractCommand.php
+++ src/Console/AbstractCommand.php
@@ -4,23 +4,36 @@ namespace MGDigital\BusQueBundle\Console;
 
 use MGDigital\BusQue\Implementation;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
-abstract class AbstractCommand extends Command
+abstract class AbstractCommand extends Command implements ContainerAwareInterface
 {
 
-    protected $container;
     private $implementationId;
+    private $container;
 
-    public function __construct(ContainerInterface $container, string $implementationId = 'busque.implementation')
+    public function __construct(string $implementationId = 'busque.implementation')
     {
         parent::__construct();
-        $this->container = $container;
         $this->implementationId = $implementationId;
+    }
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
     }
 
     protected function getImplementation(): Implementation
     {
-        return $this->container->get($this->implementationId);
+        return $this->getContainer()->get($this->implementationId);
+    }
+
+    protected function getContainer(): ContainerInterface
+    {
+        if (!$this->container instanceof ContainerInterface) {
+            throw new \BadMethodCallException('The container has not been set.');
+        }
+        return $this->container;
     }
 }

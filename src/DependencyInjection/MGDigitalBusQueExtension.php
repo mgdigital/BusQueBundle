6e087009be544c500e69b3c971182367a33dd4b0
Mike Gibson <mike@mgdigital.co.uk>
2016-06-09 12:17:02 +0100

    Removed stupid comment

--- src/DependencyInjection/MGDigitalBusQueExtension.php
+++ src/DependencyInjection/MGDigitalBusQueExtension.php
@@ -13,9 +13,6 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 class MGDigitalBusQueExtension extends Extension
 {
 
-    /**
-     * {@inheritdoc}
-     */
     public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new Configuration();
